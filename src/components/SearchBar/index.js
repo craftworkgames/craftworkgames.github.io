4e896659210a7fb100dc9668d3660b0d686da709
kian-g <kian@reconventures.com>
2026-02-26 10:24:46 -0800

    Search bar (#122)

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,6 @@
+/**
+ * Search bar component
+ *
+ * The actual SearchBar is provided by docusaurus-plugin-search-local.
+ * Custom styles are in styles.css and imported via src/css/custom.css.
+ */
